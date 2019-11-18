6748a18866a1d0bae7cc19b3a491d867944248ed
housseyn <attafi-housseyn@hotmail.fr>
2019-11-18 16:33:19 +0100

    add school

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wildcodeschool.wildandwizard.entity.School;
+
 // TODO : update this interface
 
-public interface SchoolRepository {
+public interface SchoolRepository  extends JpaRepository<School, Long> {
 }
